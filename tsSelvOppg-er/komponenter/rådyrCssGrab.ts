5209e3a414d265bc770ea588ef63e47b4d79bf2d
Martin R <martin.ristvedt@gmail.com>
2026-09-15 00:31:17 +0200

    la til ts-selvoppgave (fra et prosjekt m modul; ville bare legge til her til fremtidig bruk)

---
+++ tsSelvOppg-er/komponenter/rådyrCssGrab.ts
@@ -0,0 +1,37 @@
+export function turnStringIntoObjectArray(clipPathStr: string) {
+    return clipPathStr
+        .split("/*")
+        .map(chapter => chapter.trim())
+        .filter(chapterContent => chapterContent.length !== 0)
+        .map(chapterContent => ({
+            chapterTitle: chapterContent.substring(0, chapterContent.indexOf("*/")),
+            chapterProps: chapterContent
+                .substring(chapterContent.indexOf("*/") + 2)
+                .split(",")
+                .map(prop => prop.trim().replaceAll("%", ""))
+                .filter(prop => prop.length !== 0)
+                .map(prop => ({
+                    x: parseFloat(prop.split(" ")[0]),
+                    y: parseFloat(prop.split(" ")[1])
+                }))
+        } as cssChapter));
+}
+export function turnObjArrayToCssString(cssChapterArr: cssChapter[]): string {
+    return cssChapterArr
+        .map(chapter =>
+            `\n        /*${chapter.chapterTitle}*/ ${chapter.chapterProps
+                .map(coor => `\n        ${coor.x}% ${coor.y}%`)
+                .join(", ")}`
+        ).filter((_, i, arr) => i !== arr.length - 1)
+        .join(", ")
+        + "\n        /*END - !! dette er fra objekt til string*/";
+}
+
+type cssChapter = {
+    chapterTitle: string,
+    chapterProps: propCoordinate[]
+}
+type propCoordinate = {
+    x: number,
+    y: number
+}
---
+++ tsSelvOppg-er/main.ts
@@ -0,0 +1,27 @@
+import { turnStringIntoObjectArray, turnObjArrayToCssString } from "./komponenter/rådyrCssGrab";
+
+const deerCssStr = `
+        /*LEFT EAR INNER BASE*/
+        60% 14%,
+        /*RIGHT EAR INNER BASE*/
+        61.92% 10%,
+        63% 8%,
+        64% 5.5%,
+        64.48% 4.3%,
+        64.72% 4.2%,
+        /*RIGHT EAR TIP 1*/
+        64.72% 4.212%,
+        /*RIGHT EAR TIP 2*/
+        64.8% 4.65%,
+        64.8% 6%,
+        64.6% 7.5%,
+        63.2% 11.5%,
+        /*BASE*/
+        60.6% 14%
+        /*END*/
+`
+const cssChapterArr = turnStringIntoObjectArray(deerCssStr);
+
+console.log(cssChapterArr);
+console.log(deerCssStr);
+console.log(turnObjArrayToCssString(cssChapterArr));
